2f2a766acbea0e244e53eaf28e223db4a131341a
draindoggy <119752646+draindoggy@users.noreply.github.com>
2023-09-11 19:09:59 +0300

    Update Program.cs

--- sea_battle2.0/Program.cs
+++ sea_battle2.0/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 class Game
 {
     public static Random rand = new Random();
-
     public static void Main(string[] args)
     {
-        int[,] mas = new int[10, 10];
         int[,] mas1 = new int[10, 10];
         int[,] mas2 = new int[10, 10];
         int x, y, hitCount = 0;
+        List<string> movesHistory = new List<string>();
 
         // Инициализируем игровое поле
         for (int i = 0; i < 10; i++)
@@ -18,22 +18,12 @@ class Game
             {
                 if ((i % 2) == 0)
                 {
-                    mas[i, j] = rand.Next(0, 2);
+                    mas1[i, j] = rand.Next(0, 2);
                 }
                 else
                 {
-                    mas[i, j] = 0;
+                    mas1[i, j] = 0;
                 }
-            }
-        }
-
-        // Очищаем массивы mas1 и mas2
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                mas1[i, j] = 0;
-                mas2[i, j] = 0;
             }
         }
 
@@ -50,23 +40,24 @@ class Game
         // Цикл ввода координат кораблей и обновления игрового поля
         for (int t = 0; t < 30; t++)
         {
-            Console.WriteLine("введите координаты корабля через enter:");
+            Console.WriteLine("введите координаты корабля через Enter:");
             x = Convert.ToInt32(Console.ReadLine());
             y = Convert.ToInt32(Console.ReadLine());
             y--;
             x--;
-            mas1[x, y] = 1;
             Console.Clear();
 
             // Проверяем, попал ли игрок в корабль
-            if (mas1[x, y] == mas[x, y] && mas[x, y] == 1)
+            if (mas1[x, y] == 1)
             {
                 mas2[x, y] = 1;
                 hitCount++;
+                movesHistory.Add($"ход {t + 1}: игрок попал в корабль по координатам [{x + 1}, {y + 1}]");
             }
             else
             {
                 mas2[x, y] = 2;
+                movesHistory.Add($"ход {t + 1}: игрок промахнулся по координатам [{x + 1}, {y + 1}]");
             }
 
             // Отображаем обновленное игровое поле
@@ -91,9 +82,16 @@ class Game
                 }
                 Console.WriteLine();
             }
-        Console.ForegroundColor= ConsoleColor.Green;
-        Console.WriteLine("количество попаданий: " + hitCount);
-        Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("количество попаданий: " + hitCount);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nИстория ходов игрока:");
+            foreach (var move in movesHistory)
+            {
+                Console.WriteLine(move);
+            }
+            Console.ResetColor();
         }
         Console.Read();
     }
